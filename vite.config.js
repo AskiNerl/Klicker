6885f537aec9796169d93c2c2c361e83e99cace0
Влад <vlados@example.com>
2026-05-25 19:17:13 +0300

    Добавляю билд для деплоя

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Klicker/', 
+  base: '/Klicker/', // Это имя твоего репозитория на GitHub
 })
